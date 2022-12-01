2619718f5d4e519cba311c88b3ad133825891976
sarabhat <sarabhat@gmail.com>
2022-12-01 10:02:02 -0800

    " SB Validate Registration firsname field"

---
+++ src/test/java/definitions/SaraBXPathLibrary.java
@@ -0,0 +1,25 @@
+package definitions;
+
+public class SaraBXPathLibrary {
+    public static String sURLXpath = "http://ask-internship.portnov.com/#/login";
+    public static String sEmailXpath = "//input[@formcontrolname='email']";
+    public static String sPasswordXpath = "//input[@formcontrolname='password']";
+    public static String sSubmitButtonXpath = "//button[@type='submit']";
+    public static String sSignInButtonXpath ="//span[text()='Sign In']";
+    public static String sRegisterNowButtonXpath ="//span[contains(text(),'Register Now')]";
+    public static String sFirstNameXpath ="//input[@formcontrolname='firstName']";
+    public static String SLastNameXpath ="//input[@formcontrolname='lastName']";
+    public static String sGroupCodexPath ="//input[@formcontrolname='group']";
+    public static String sConfirmedPasswordxPath ="//input[@formcontrolname='confirmPassword']";
+    public static String sRegisterMeButtonXpath ="//span[contains(text(),'Register Me')]";
+    public static String sRegistrationXpath ="//h4[contains(text(),'Registration')]";
+    public static String sErrorMessageXpath = "//mat-error[contains(text(),'This field is required')]";
+    public static String sBackToLoginBtnXpath = "//span[contains(text(),'Back to Login')]";
+    public static String sErrorMessage2Xpath = "//mat-error[contains(text(),'Should contain only latin characters')]";
+
+
+
+
+
+
+}
