fc6b53f0b8b60a3f4a542a99700ab335e7d15803
KTSTanya <nbz.cnmn@gmail.com>
2022-12-01 20:17:36 -0800

    Test scenario: test created at validation

--- src/test/java/definitions/TanyaKStepDefsQuizCreatedAt.java
+++ src/test/java/definitions/TanyaKStepDefsQuizCreatedAt.java
@@ -1,9 +1,91 @@
 package definitions;
 
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
+import org.openqa.selenium.By;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import static support.TestContext.getDriver;
+
 
 public class TanyaKStepDefsQuizCreatedAt {
-    @Then("TK type")
-    public void tkType() {
+    @Given("TK navigate to {string}")
+    public void tkNavigateTo(String sURL) {getDriver().get(sURL);
+    }
+
+    @Then("TK type {string} as Email Address")
+    public void tkTypeAsEmailAddress(String sEmailAddress) {
+        getDriver().findElement(By.xpath("//input[@formcontrolname='email']")).sendKeys(sEmailAddress);
+    }
+
+    @And("TK type {string} as Password")
+    public void tkTypeAsPassword(String sPassword) {
+        getDriver().findElement(By.xpath("//input[@formcontrolname='password']")).sendKeys(sPassword);
+
+    }
+
+    @And("TK click on Sign In button")
+    public void tkClickOnSignInButton() {
+        getDriver().findElement(By.xpath("//button[@type='submit']")).click();
+    }
+
+    @Then("TK wait for {int} seconds")
+    public void avWaitForSeconds(long iSec) throws InterruptedException {
+        Thread.sleep(1000*iSec);
+    }
+
+    @Then("TK click on {string} menu item")
+    public void avClickOnMenuItem(String sMenuItem) {
+        getDriver().findElement(By.xpath("//h5[contains(text(),'"+sMenuItem+"')]")).click();
+    }
+
+    @Then("TK click on {string} button")
+    public void tkClickOnButton(String sButtonName) {
+        getDriver().findElement(By.xpath("//span[contains(text(),'"+sButtonName+"')]")).click();
+    }
+
+    @Then("TK type {string} as Quiz Title")
+    public void tkTypeAsQuizTitle(String sQuizeTitle) {
+        getDriver().findElement(By.xpath("//input[@placeholder='Title Of The Quiz *']")).sendKeys(sQuizeTitle);
+    }
+
+    @Then("TK add a question")
+    public void tkAddAQuestion() {
+        getDriver().findElement(By.xpath("//mat-icon[contains(text(),'add_circle')]")).click();
+
+    }
+
+    @Then("TK choose {string} question type")
+    public void avChooseQuestionType(String sQuestionChoice) {
+
+        getDriver().findElement(By.xpath("//*[contains(text(),'"+sQuestionChoice+"')]")).click();
+    }
+
+    @Then("TK type {string} as {string}")
+    public void avTypeAs(String sFieldValue, String sFieldPosition) {
+        getDriver().findElement(By.xpath("//textarea[@placeholder='" + sFieldPosition + "*']")).sendKeys(sFieldValue);
+    }
+
+    @Then("TK click on List of Quizzes")
+    public void tkClickOnListOfQuizzes() {
+        getDriver().findElement(By.xpath("//mat-panel-title[contains(text(),'Where we can find a Xpath?')]")).click();
+    }
+
+    @Then("TK validate created at value")
+    public void tkValidateCreatedAtValue() {
+        org.openqa.selenium.WebElement el = getDriver().findElement(By.xpath("/html[1]/body[1]/ac-root[1]/mat-sidenav-container[1]/mat-sidenav-content[1]/main[1]/ac-quizzes-list[1]/mat-card[1]/div[1]/mat-accordion[1]/mat-expansion-panel[129]/div[1]/div[1]/div[1]/table[1]/tbody[1]/tr[4]/td[2]"));
+        String el_value = el.getText();
+        System.out.println(el_value);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+        dateFormat.setLenient(false);
+        try {
+            dateFormat.parse(el_value);
+        } catch (ParseException pe) {
+            System.out.println("Oi!!");
+            return;
+        }
+        System.out.println("Good!");
     }
 }
--- src/test/resources/features/TanyaK_QuizCeatedAt.feature
+++ src/test/resources/features/TanyaK_QuizCeatedAt.feature
@@ -1,10 +1,29 @@
-#Exercise for ASK automation
-#Author: TanyaK
-@homepage
-Feature: ASK application
+# Automate test set "Quiz Created at"
+# https://jira.portnov.com/browse/ACD-606
+#  Author: Tanya Kalinina
 
-  @homepage1
-  Scenario: Open ASK application
+@CreatedAt
+Feature: testing Quiz Created at
+
+  @CreatedAt1
+  Scenario: Login as a teacher
     Given TK navigate to "http://ask-internship.portnov.com/#/login"
-    Then TK type
-    And TK click
+    Then TK type "gvardiaditan@gmail.com" as Email Address
+    And TK type "Voznesenskaya_dom_1_120" as Password
+    And TK click on Sign In button
+    Then TK wait for 5 seconds
+    Then TK click on "Quizzes" menu item
+    Then TK wait for 1 seconds
+    Then TK click on "Create New Quiz" button
+    Then TK wait for 1 seconds
+    Then TK type "Where we can find a Xpath?" as Quiz Title
+    Then TK wait for 5 seconds
+    Then TK add a question
+    Then TK choose "Textual" question type
+    Then TK type "How do you define Xpath? " as "Question "
+    Then TK wait for 5 seconds
+    Then TK click on "Save" button
+    Then TK wait for 5 seconds
+    Then TK click on List of Quizzes
+    Then TK wait for 5 seconds
+    Then TK validate created at value
