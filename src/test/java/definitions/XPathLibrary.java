20b871629f3ecd73e44af1796a29037b73c3895b
wattemma <wattcemma@gmail.com>
2022-12-02 17:13:54 -0600

    only my stuff

--- src/test/java/definitions/XPathLibrary.java
+++
@@ -1,8 +0,0 @@
-package definitions;
-
-public class XPathLibrary {
-    public static String sEmailXpath = "//input[@formcontrolname='email']";
-    public static String sPasswordXpath = "//input[@formcontrolname='password']";
-    public static String sSubmitButtonXpath = "//button[@type='submit']";
-
-}
