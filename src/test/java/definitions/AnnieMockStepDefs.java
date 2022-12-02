073e39351f18d2baaa358c09e49fd0104341f650
anne2900 <lydi2900@gmail.com>
2022-12-01 23:01:26 -0500

    AnnP:Adding Login Scenario and Def

--- src/test/java/definitions/AnnieMockStepDefs.java
+++ src/test/java/definitions/AnnieMockStepDefs.java
@@ -1,9 +1,49 @@
 package definitions;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import org.openqa.selenium.By;
+
 import static support.TestContext.getDriver;
 public class AnnieMockStepDefs {
     @Given("I open URL {string}")
     public void iOpenURL(String url) {
         getDriver().get(url);
+    }
+
+      @And("I type {string} as email")
+    public void iTypeAsEmail(String email) {
+        getDriver().findElement(By.xpath("//input[@formcontrolname='email']")).sendKeys(email);
+    }
+
+    @And("I type {string} as password")
+    public void iTypeAsPassword(String passwd) {
+        getDriver().findElement(By.xpath("//input[@formcontrolname='password']")).sendKeys(passwd);
+    }
+/*
+    @Then("I should see the page title as {string}")
+    public void iShouldSeeThePageTitleAs(String expectedTitle) {
+      String actualTitle= getDriver().getTitle();
+      if(actualTitle.equalsIgnoreCase(expectedTitle))
+          System.out.println("Title Matched");
+      else
+          System.out.println("Title did not match");
+    }
+
+    @Then("I should be logged into the {string}")
+    public void iShouldBeLoggedIntoThe(String url) {
+        getDriver().get(url);
+    }
+ */
+    @And("I click on the Sign In button")
+    public void iClickOnTheSignInButton() {
+        getDriver().findElement(By.xpath("//button[@type='submit']")).click();
+    }
+
+
+
+    @Then("I wait for {int} sec")
+    public void iWaitForSec(int t) throws InterruptedException{
+        Thread.sleep(t*1000);
     }
 }
--- src/test/resources/features/Annie_P_trial.feature
+++ src/test/resources/features/Annie_P_trial.feature
@@ -1,4 +1,13 @@
 #Annie
   Feature: ASK App
     Scenario: Launch the Application
-      Given I open URL "https://ask-internship.portnov.com/#/login"
+      Given I open URL "http://ask-internship.portnov.com/#/login"
+      And I type "dhah@tmsave.com" as email
+      And I type "Trichy75" as password
+      Then I wait for 3 sec
+      And I click on the Sign In button
+     Then I wait for 3 sec
+
+
+
+
